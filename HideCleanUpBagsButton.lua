07b475b822539c115e710f3531bae6a48383c912
cmc3215 <cmc3215@fb933498-b5ea-4928-bc49-a4eea654036a>
2015-06-23 08:29:10 +0000

    Patch 6.2 updates

--- HideCleanUpBagsButton.lua
+++ HideCleanUpBagsButton.lua
@@ -1,21 +1,20 @@
 -- Slash Command Handler
 function HCUBB_SlashCmdHandler( msg )
 	if not HCUBB_SAVEDVARIABLES then
-		HCUBB_SAVEDVARIABLES = {};
+		HCUBB_SAVEDVARIABLES = {}; -- Initialize SavedVariables
 	end
 	--
-	if msg == "bank hide" then
-		HCUBB_SAVEDVARIABLES["bank"] = "hide";
-		BankItemAutoSortButton:Hide();
-		BankItemAutoSortButton:Show();
-		print( "Hiding clean up bank/reagent bank button" );
-	elseif msg == "bank show" then
-		HCUBB_SAVEDVARIABLES["bank"] = "show";
-		BankItemSearchBox:SetWidth( 124 );
-		BankItemSearchBox:SetPoint( "TOPRIGHT", "$parent", "TOPRIGHT", -44, -33 );
-		BankItemAutoSortButton:Show();
-		print( "Showing clean up bank/reagent bank button" );
+	if msg == "bank hide" or msg == "bank show" then
+		-- Hide or Show - Bank/Reagent Bank "Clean Up" button
+		if msg == "bank hide" then
+			HCUBB_SAVEDVARIABLES["bank"] = "hide";
+		else
+			HCUBB_SAVEDVARIABLES["bank"] = "show";
+		end
+		BankItemAutoSortButton:Hide(); -- Hide and Show to force OnShow script to fire
+		BankItemAutoSortButton:Show(); -- ^
 	else
+		-- Print slash commands
 		print( NORMAL_FONT_COLOR_CODE .. "Hide Clean Up Bags Button" .. FONT_COLOR_CODE_CLOSE );
 		print( NORMAL_FONT_COLOR_CODE .. "- /hcubb bank hide" .. FONT_COLOR_CODE_CLOSE );
 		print( NORMAL_FONT_COLOR_CODE .. "- /hcubb bank show" .. FONT_COLOR_CODE_CLOSE );
@@ -29,15 +28,16 @@ SlashCmdList["HCUBB"] = HCUBB_SlashCmdHandler;
 
 -- Clean Up Bags Button
 BagItemAutoSortButton:HookScript( "OnShow", function( self )
-	BagItemSearchBox:SetWidth( 124 );
+	BagItemSearchBox:SetWidth( 124 ); -- Hide: SearchBox extended to cover where "Clean Up" button would normally appear
 	self:Hide();
 end );
 
 -- Clean Up Bank/Reagent Bank Button
 BankItemAutoSortButton:HookScript( "OnShow", function( self )
 	if not HCUBB_SAVEDVARIABLES or HCUBB_SAVEDVARIABLES["bank"] == "hide" then
-		BankItemSearchBox:SetWidth( 124 );
-		BankItemSearchBox:SetPoint( "TOPRIGHT", "$parent", "TOPRIGHT", -15, -33 );
+		BankItemSearchBox:SetPoint( "TOPRIGHT", "$parent", "TOPRIGHT", -15, -33 ); -- Hide: SearchBox moved to the right where "Clean Up" button would normally appear
 		self:Hide();
+	else
+		BankItemSearchBox:SetPoint( "TOPRIGHT", "$parent", "TOPRIGHT", -44, -33 ); -- Show: SearchBox moved back to the left, into original position
 	end
 end );
